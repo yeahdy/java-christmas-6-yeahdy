3f9dc694730afeac98c77f128ce967a9d34b0199
yeahdy <yejine98@naver.com>
2023-11-12 22:30:06 +0900

    Feat(PrintUtils.class): UI 출력 기능 메소드 정의

---
+++ src/main/java/utils/PrintUtils.java
@@ -0,0 +1,20 @@
+package utils;
+
+import constants.ErrorCodeConstant;
+
+public class PrintUtils {
+
+    public static void println(String message){
+        System.out.println(message);
+    }
+    public static void print(String message){
+        System.out.print(message);
+    }
+    public static void printf(String formatMessage, Object args){
+        System.out.printf("%n"+formatMessage+"%n",args);
+    }
+    public static void errorPrint(String errorMessage){
+        System.out.println(ErrorCodeConstant.ERROR + errorMessage);
+    }
+
+}
