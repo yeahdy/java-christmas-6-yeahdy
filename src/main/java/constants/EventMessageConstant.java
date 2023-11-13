4bfe370ad6b68695857b1d550fd93cc1cc892ba9
yeahdy <yejine98@naver.com>
2023-11-13 14:40:29 +0900

    Feat: UI에 표출 이벤트 메세지 정의

---
+++ src/main/java/constants/EventMessageConstant.java
@@ -0,0 +1,17 @@
+package constants;
+
+public class EventMessageConstant {
+
+    public static final String GREETING = "안녕하세요! 우테코 식당 12월 이벤트 플래너입니다.";
+    public static final String WHEN_IS_THE_DATE = "12월 중 식당 예상 방문 날짜는 언제인가요? (숫자만 입력해 주세요!)";
+    public static final String WHAT_IS_THE_MENU = "주문하실 메뉴를 메뉴와 개수를 알려 주세요. (e.g. 해산물파스타-2,레드와인-1,초코케이크-1)";
+    public static final String CHECK_BENEFITS = "12월 {date}일에 우테코 식당에서 받을 이벤트 혜택 미리 보기!";
+    public static final String ORDER_MENU = "<주문 메뉴>";
+    public static final String PRICE_BEFORE_DISCOUNT = "<할인 전 총주문 금액>";
+    public static final String GIFT_MENU = "<증정 메뉴>";
+    public static final String BENEFITS_LIST = "<혜택 내역>";
+    public static final String TOTAL_BENEFITS_PRICE = "<총혜택 금액>";
+    public static final String PRICE_AFTER_DISCOUNT = "<할인 후 예상 결제 금액>";
+    public static final String EVENT_BADGE = "<12월 이벤트 배지>";
+
+}
