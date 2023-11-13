26b5f10de0f60090015107ead2f455f288f66219
yeahdy <yejine98@naver.com>
2023-11-13 17:24:21 +0900

    Feat: 예약 날짜 범위 유효성 검사 메소드 구현

--- src/main/java/validators/EventValidator.java
+++ src/main/java/validators/EventValidator.java
@@ -1,13 +1,24 @@
 package validators;
 
 import constants.ErrorCodeConstant;
+
 public class EventValidator {
 
+    public static final int MIN_DATE_NUMBER = 1;
+    public static final int MAX_DATE_NUMBER = 31;
+
     /** 숫자로 변환이 안될 경우 예외 발생 */
-    public static void validateNumber(String characters){
+    public static int validateNumber(String characters){
         try{
-            Integer.parseInt(characters);
+            return Integer.parseInt(characters);
         }catch (IllegalArgumentException iae){
+            throw new IllegalArgumentException(ErrorCodeConstant.NOT_VALID_DATE_ERROR);
+        }
+    }
+
+    /** 정수가 기준을 벗어날 경우 예외 발생 */
+    public static void validateBetweenDate(int number) {
+        if (number < MIN_DATE_NUMBER || number > MAX_DATE_NUMBER) {
             throw new IllegalArgumentException(ErrorCodeConstant.NOT_VALID_DATE_ERROR);
         }
     }
