d535507e4f0260c30a762c30afae4f0b4568e748
yeahdy <yejine98@naver.com>
2023-11-14 01:36:21 +0900

    Feat(UserReservation): 회원예약 도메인 생성

--- src/main/java/christmas/Application.java
+++ src/main/java/christmas/Application.java
@@ -2,6 +2,7 @@ package christmas;
 
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        ChristmasEventController christmasEventController = new ChristmasEventController();
+        christmasEventController.run();
     }
 }
---
+++ src/main/java/domain/UserReservation.java
@@ -0,0 +1,15 @@
+package domain;
+
+import java.util.List;
+
+public class UserReservation {
+
+    private final ReservationDate reservationDate;
+    private final List<ReservationMenu> menuList;
+
+    public UserReservation(ReservationDate reservationDate, List<ReservationMenu> menuList){
+        this.reservationDate = reservationDate;
+        this.menuList = menuList;
+    }
+
+}
