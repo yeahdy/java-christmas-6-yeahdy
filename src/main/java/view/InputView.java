440b33b151be00e31e8efb2f939d0a2f17295a8e
yeahdy <yejine98@naver.com>
2023-11-14 00:46:25 +0900

    Feat(InputView): 사용자 주문 입력받기 메소드 생성

--- src/main/java/view/InputView.java
+++ src/main/java/view/InputView.java
@@ -1,10 +1,12 @@
 package view;
 
+import static constants.EventMessageConstant.WHAT_IS_THE_MENU;
 import static constants.EventMessageConstant.WHEN_IS_THE_DATE;
 
 import camp.nextstep.edu.missionutils.Console;
 import utils.PrintUtils;
 import validators.ReservationDateValidator;
+import validators.ReservationMenuValidator;
 
 public class InputView {
 
@@ -13,6 +15,13 @@ public class InputView {
 
         String date = Console.readLine();
         return ReservationDateValidator.validateNumber(date);
+    }
+
+    public String readOrder() {
+        PrintUtils.println(WHAT_IS_THE_MENU);
+
+        String order = Console.readLine();
+        return ReservationMenuValidator.validateString(order);
     }
 
 }
