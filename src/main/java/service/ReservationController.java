802415f176b95595e6010c9158e74c1686d16e28
yeahdy <yejine98@naver.com>
2023-11-14 01:39:31 +0900

    Fix: 이벤트 예약 서비스 클래스명 변경
    - EventReservationService > ReservationController

--- src/main/java/service/ReservationController.java
+++ src/main/java/service/ReservationController.java
@@ -1,24 +1,24 @@
 package service;
 
 import domain.ReservationDate;
+import domain.ReservationMenu;
+import java.util.List;
 import utils.PrintUtils;
 import view.InputView;
 import view.OutputView;
 
-public class EventReservationService {
+public class ReservationController {
 
     private InputView inputView = new InputView();
     private OutputView outputView = new OutputView();
-    private ReservationDate reservationDate;
     private ReservationMenuService reservationMenuService = new ReservationMenuService();
 
-    public void createReservationDate(){
+    public ReservationDate createReservationDate(){
         outputView.printGreeting();
         while (true){
             try{
                 int date = inputView.readDate();
-                reservationDate = new ReservationDate(date);
-                break;
+                return new ReservationDate(date);
             }catch (IllegalArgumentException iae){
                 PrintUtils.errorPrint(iae.getMessage());
             }
@@ -26,18 +26,17 @@ public class EventReservationService {
     }
 
 
-    public void createReservationMenu(){
+    public List<ReservationMenu> createReservationMenu(){
         while (true){
             try{
                 String order = inputView.readOrder();
                 String[] orderList = reservationMenuService.validateOrderList(order);
-                reservationMenuService.validateReservationMenu(orderList);
-                break;
+
+                return reservationMenuService.validateReservationMenu(orderList);
             }catch (IllegalArgumentException iae){
                 PrintUtils.errorPrint(iae.getMessage());
             }
-        }
-
+        } //while
     }
 
 }
