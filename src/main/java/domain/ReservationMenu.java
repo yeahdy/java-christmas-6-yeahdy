432817d65b6e6e5c7283986d925bd2e95c197de2
yeahdy <yejine98@naver.com>
2023-11-14 01:23:16 +0900

    Feat(ReservationMenu): 주문예약 도메인 로직 유효성 검증 메소드 구현

--- src/main/java/domain/ReservationMenu.java
+++ src/main/java/domain/ReservationMenu.java
@@ -1,5 +1,8 @@
 package domain;
 
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
 import validators.ReservationMenuValidator;
 
 public class ReservationMenu {
@@ -10,23 +13,39 @@ public class ReservationMenu {
 
     public ReservationMenu(){}
 
-
-    /** 사용자 메뉴 입력 유효성 검사
-     *
-     * @param orders "티본스테이크-1,바비큐립-1,초코케이크-2,제로콜라-1" 문자열 타입
-     * @return {"티본스테이크-1","바비큐립-1","초코케이크-2","제로콜라-1"} 문자열 배열 타입
-     */
-    public String[] validateOrder(String orders){
-        String[] orderList = orders.split(",");
-
-        for(String order : orderList){
-            ReservationMenuValidator.validateString(order);
-            String[] orderFormat = ReservationMenuValidator.validateOrderFormat(order);
-            ReservationMenuValidator.validateOrderCount(orderFormat);
-        }
-
-        return orderList;
+    private ReservationMenu(Menu menu, int count){
+        this.menu = menu;
+        this.count = count;
     }
 
+    /** 메뉴-수량 유효성 검사 */
+    public void validateOrder(String order){
+        ReservationMenuValidator.validateString(order);
+        String[] orderFormat = ReservationMenuValidator.validateOrderFormat(order);
+        ReservationMenuValidator.validateOrderCount(orderFormat);
+    }
+
+    /** 중복 메뉴를 입력한 경우 true 반환, 중복아닐 경우 false */
+    public boolean isDuplicatedMenu(String[] orderList){
+        List<String> menuList = Arrays.stream(orderList)
+                .map(value -> value.split("-")[0])
+                .collect(Collectors.toList());
+
+        return ReservationMenuValidator.isDuplicatesList(menuList);
+    }
+
+    /** 고객이 메뉴판에 없는 메뉴를 입력한 경우 */
+    public ReservationMenu getValidatedReservationMenu(String order){
+        String[] foodAndCount = order.split("-");
+        String food = foodAndCount[0];
+        int count = Integer.parseInt(foodAndCount[1]);
+
+        Menu menu = Menu.getMenu(food);
+        if(menu == null){
+            return null;
+        }
+
+        return new ReservationMenu(menu,count);
+    }
 
 }
