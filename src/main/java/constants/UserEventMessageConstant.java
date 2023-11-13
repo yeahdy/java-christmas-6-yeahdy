ffd3e804a9ba2c801b7b5014b1ec5d2e4a8ed5a9
yeahdy <yejine98@naver.com>
2023-11-13 14:55:57 +0900

    Feat: UI에 표출할 사용자 입력 메세지 정의

---
+++ src/main/java/constants/UserEventMessageConstant.java
@@ -0,0 +1,10 @@
+package constants;
+
+public class UserEventMessageConstant {
+
+    public static final String ORDER_MENU_AND_COUNT = "%s %d개";
+    public static final String MENU_PRICE = "%s원";
+    public static final String NOTHING = "없음";
+    public static final String BENEFITS_PRICE_LIST = "%s: -%s원";
+
+}
