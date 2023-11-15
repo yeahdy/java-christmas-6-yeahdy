7bada654433df9616a558440c31765402225eb05
yeahdy <yejine98@naver.com>
2023-11-15 13:36:16 +0900

    Feat: 사용자 UI OutputView 에 출력할 모든 메세지에 대한 메소드 구현

--- src/main/java/view/OutputView.java
+++ src/main/java/view/OutputView.java
@@ -1,19 +1,54 @@
 package view;
 
 
+import static constants.EventMessageConstant.BENEFITS_LIST;
+import static constants.EventMessageConstant.CHECK_BENEFITS;
+import static constants.EventMessageConstant.EVENT_BADGE;
+import static constants.EventMessageConstant.GIFT_MENU;
 import static constants.EventMessageConstant.GREETING;
 import static constants.EventMessageConstant.ORDER_MENU;
+import static constants.EventMessageConstant.PRICE_AFTER_DISCOUNT;
+import static constants.EventMessageConstant.PRICE_BEFORE_DISCOUNT;
+import static constants.EventMessageConstant.TOTAL_BENEFITS_PRICE;
 
 import utils.PrintUtils;
 
 public class OutputView {
 
-    public void printGreeting(){
+    public void printGreeting() {
         PrintUtils.println(GREETING);
+    }
+
+    public void printCheckBenefit(String date) {
+        PrintUtils.replacePrintf(CHECK_BENEFITS, date);
     }
 
     public void printOrderMenu() {
         PrintUtils.println(ORDER_MENU);
+    }
+
+    public void printPriceBeforeDiscount() {
+        PrintUtils.println(PRICE_BEFORE_DISCOUNT);
+    }
+
+    public void printGiftMenu() {
+        PrintUtils.println(GIFT_MENU);
+    }
+
+    public void printBenefitsList() {
+        PrintUtils.println(BENEFITS_LIST);
+    }
+
+    public void printTotalBenefitsPrice() {
+        PrintUtils.println(TOTAL_BENEFITS_PRICE);
+    }
+
+    public void printPriceAfterDiscount() {
+        PrintUtils.println(PRICE_AFTER_DISCOUNT);
+    }
+
+    public void printEventBadge() {
+        PrintUtils.println(EVENT_BADGE);
     }
 
 }
