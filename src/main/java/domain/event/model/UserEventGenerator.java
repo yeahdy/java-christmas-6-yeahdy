867ce92f52d0775723fb8621613c2c5776f2d888
yeahdy <yejine98@naver.com>
2023-11-15 20:47:15 +0900

    Feat: 증정 메뉴, 혜택 내역 문구 가공 메소드 구현

--- src/main/java/domain/event/model/UserEventGenerator.java
+++ src/main/java/domain/event/model/UserEventGenerator.java
@@ -8,14 +8,16 @@ import java.util.List;
 
 public class UserEventGenerator {
 
-    /** 주문 메뉴 */
-    public List<String> getUserMenuList(List<ReservationMenu> reservationMenuList){
+    /**
+     * 주문 메뉴
+     */
+    public List<String> getUserMenuList(List<ReservationMenu> reservationMenuList) {
         List<String> userOrderMenuList = new ArrayList<>();
 
-        for(ReservationMenu reservationMenu : reservationMenuList){
+        for (ReservationMenu reservationMenu : reservationMenuList) {
             String message = UserEventMessageConstant.ORDER_MENU_AND_COUNT;
-            message = message.replace("{food}",reservationMenu.getMenu().getFood());
-            message = message.replace("{count}",String.valueOf(reservationMenu.getCount()));
+            message = message.replace("{food}", reservationMenu.getMenu().getFood());
+            message = message.replace("{count}", String.valueOf(reservationMenu.getCount()));
 
             userOrderMenuList.add(message);
         }
@@ -23,17 +25,63 @@ public class UserEventGenerator {
         return userOrderMenuList;
     }
 
-    /** 총혜택 금액 */
-    public String getMenuPrice(int price){
-        DecimalFormat decimalFormat = new DecimalFormat("#,###");
-        String formattedNumber = decimalFormat.format(price);
-
+    /**
+     * 총혜택 금액
+     */
+    public String getMenuPrice(int price) {
         String message = UserEventMessageConstant.MENU_PRICE;
-        return message.replace("{price}",String.valueOf(formattedNumber));
+        return message.replace("{price}", getCommaPrice(price));
     }
 
-    /** 증정 메뉴 */
+    /**
+     * 증정 메뉴
+     */
+    public String getUserGift(String gift, int count) {
+        String message = UserEventMessageConstant.ORDER_MENU_AND_COUNT;
+        message = message.replace("{food}", gift);
+        message = message.replace("{count}", String.valueOf(count));
+        return message;
+    }
 
-    /** 혜택 내역 */
+    /**
+     * 혜택 내역
+     */
+    public List<String> getBenefitsList(EventDiscount eventDiscount) {
+        List<String> benefitsList = new ArrayList<>();
+        if (eventDiscount.getChristmasPrice() != 0) {
+            String message = UserEventMessageConstant.BENEFITS_PRICE_LIST
+                    .replace("{event}", UserEventMessageConstant.CHRISTMAS_DISCOUNT);
+            message = message.replace("{price}", getCommaPrice(eventDiscount.getChristmasPrice()));
+            benefitsList.add(message);
+        }
+
+        if (eventDiscount.getWeekdayPrice() != 0) {
+            String message = UserEventMessageConstant.BENEFITS_PRICE_LIST
+                    .replace("{event}", UserEventMessageConstant.WEEKDAY_DISCOUNT);
+            message = message.replace("{price}", getCommaPrice(eventDiscount.getWeekdayPrice()));
+            benefitsList.add(message);
+        }
+
+        if (eventDiscount.getWeekendPrice() != 0) {
+            String message = UserEventMessageConstant.BENEFITS_PRICE_LIST
+                    .replace("{event}", UserEventMessageConstant.WEEKEND_DISCOUNT);
+            message = message.replace("{price}", getCommaPrice(eventDiscount.getWeekendPrice()));
+            benefitsList.add(message);
+        }
+
+        if (eventDiscount.getGiftPrice() != 0) {
+            String message = UserEventMessageConstant.BENEFITS_PRICE_LIST
+                    .replace("{event}", UserEventMessageConstant.GIFT_DISCOUNT);
+            message = message.replace("{price}", getCommaPrice(eventDiscount.getGiftPrice()));
+            benefitsList.add(message);
+        }
+
+        return benefitsList;
+    }
+
+    private String getCommaPrice(int price) {
+        DecimalFormat decimalFormat = new DecimalFormat("#,###");
+        return decimalFormat.format(price);
+    }
 
 }
